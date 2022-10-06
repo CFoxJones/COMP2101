8fb5534c9c9d58b4a6b6ed4615ab4e384357f92d
chandler <200488757@student.georgianc.on.ca>
2022-10-06 10:17:30 -0400

    Lab 2 Complete

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
-#Showing the systems FQDN
-echo "FQDN:"
-hostname 
-#Showing the OS name and version
-hostnamectl 
-#Showing an IP Address
-echo "IP Address:"
-hostname --all-ip-addresses 
-#Showing the amount of space only in the root filesystem
-echo "Root Filesystem Status:"
-df /root -h
+
+#gathering data
+#Creating  fqdn variable to hold FQDN
+fqdn=$(hostname -f )
+#Creating osversion variable
+source /etc/os-release
+#Creating Ip variable
+ip=$(hostname --all-ip-addresses)
+#Creating Filesystem free space variable
+harddrive=$(df -h --output=avail / | grep Avail -v)
+
+#Using an output template
+cat <<eof
+
+Report Lab 2 For $HOSTNAME
+==========================
+FQDN: $fqdn
+Operating System name and version: $PRETTY_NAME
+IP Address: $ip
+Root Filesystem Free Space: $harddrive
+=========================
+eof
+
